7bd354b5ed606a1a81c80742aa67f8cfe84c1e35
Badwan <badwan_a@outlook.com>
2023-07-29 23:53:02 +0300

    Added husky and lint-staged configuration

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-  .setTitle('NestJS Project API')
-  .setDescription('The NestJS Project API')
-  .setVersion('1.0')
-  .build();
+    .setTitle('NestJS Project API')
+    .setDescription('The NestJS Project API')
+    .setVersion('1.0')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
